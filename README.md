67d486d1a592445932196bedd70f863b05b6fda4
pawel-laptop <p.blaszczy@gmail.com>
2024-06-14 20:00:48 +0200

    make canvas responsive

--- README.md
+++ README.md
@@ -1,23 +1,34 @@
 # Info
+
 Webpage is hosted on https://pert.oncriticalpath.com/
 
 # Local installation
-* clone repository
+
+- clone repository
 
       git clone https://github.com/plpabla/pertool-front.git
 
-* install dependencies
+- install dependencies
 
       npm install
 
-* set up backend connectivity in `./src/Backend.js` file (*note: you can use that one as it is hosted on Azure cloud*)
+- set up backend connectivity in `./src/Backend.js` file (_note: you can use that one as it is hosted on Azure cloud_)
 
       static #url = 'https://pert.azurewebsites.net'
 
-* create a bundle
+- create a bundle
 
       npm build
 
-* run backend if you use local one (it should support endpoints which are defined in `./src/Backend.js`)
+- run backend if you use local one (it should support endpoints which are defined in `./src/Backend.js`)
 
-* run - you can use VSCode Live Sever
+- run - you can use VSCode Live Sever
+
+# Develop
+
+- set watch mode
+  npm watch
+
+- start live server
+
+- enjoy
--- src/app.js
+++ src/app.js
@@ -1,95 +1,94 @@
-"use strict"
-import Konva from 'konva';
-import Editor from './Editor';
-import Model from './Model';
-import setAlertMessage from './alert';
+"use strict";
+import Konva from "konva";
+import Editor from "./Editor";
+import Model from "./Model";
+import setAlertMessage from "./alert";
 
-let sceneWidth = 1000;
-let sceneHeight = 700;
+let sceneWidth = window.innerWidth * 0.9;
+let sceneHeight = window.innerHeight * 0.8;
 const stage = new Konva.Stage({
-    height: sceneHeight,
-    width: sceneWidth,
-    container: "canvas",
-    draggable: false             // TODO: make draggable with mid mouse button pressed
+  height: sceneHeight,
+  width: sceneWidth,
+  container: "canvas",
+  draggable: false, // TODO: make draggable with mid mouse button pressed
+});
+const border = new Konva.Rect({
+  x: 2,
+  y: 2,
+  width: stage.width() - 4,
+  height: stage.height() - 4,
+  stroke: "black",
+  strokeWidth: 2,
 });
 
 const layer = new Konva.Layer();
 stage.add(layer);
 
-layer.add(new Konva.Rect({
-    x:2,
-    y:2,
-    width: stage.width()-4,
-    height: stage.height()-4,
-    stroke: "black",
-    strokeWidth: 2
-}));
+layer.add(border);
 
 const editor = new Editor(stage, calculate);
 
-window.onload = function() {
-    fitStageIntoParentContainer();
-    // adapt the stage on any window resize
-    window.addEventListener('resize', fitStageIntoParentContainer);
-    
-    document.getElementById("btn-save").addEventListener("click", saveModel);
-    document.getElementById("btn-load").addEventListener("click", loadModel);
-    document.getElementById("btn-export").addEventListener("click", exportModel);
-    document.getElementById("btn-import").addEventListener("click", importModel);
-    document.getElementById("btn-clear").addEventListener("click", clearModel);
+window.onload = function () {
+  fitStageIntoParentContainer();
+  // adapt the stage on any window resize
+  window.addEventListener("resize", fitStageIntoParentContainer);
 
-    calculate();
+  document.getElementById("btn-save").addEventListener("click", saveModel);
+  document.getElementById("btn-load").addEventListener("click", loadModel);
+  document.getElementById("btn-export").addEventListener("click", exportModel);
+  document.getElementById("btn-import").addEventListener("click", importModel);
+  document.getElementById("btn-clear").addEventListener("click", clearModel);
+
+  calculate();
 };
 
-
 function fitStageIntoParentContainer() {
-    var container = document.querySelector('#canvas-parent');
+  let sceneWidth = window?.innerWidth * 0.9 ?? 1000;
+  let sceneHeight = window?.innerHeight * 0.8 ?? 700;
 
-    // now we need to fit stage into parent container
-    var containerWidth = container.offsetWidth;
-
-    // but we also make the full scene visible
-    // so we need to scale all objects on canvas
-    var scale = containerWidth / sceneWidth;
-
-    stage.width(sceneWidth * scale);
-    stage.height(sceneHeight * scale);
-    stage.scale({ x: scale, y: scale });
+  stage.width(sceneWidth);
+  stage.height(sceneHeight);
+  border.width(sceneWidth - 4);
+  border.height(sceneHeight - 4);
 }
 
 async function calculate(e) {
-    let res = await editor.calculate();
-    
-    if(res.res==='ok') {
-        setAlertMessage("Critical path calculated", "info")
-        return 0
-    } else {
-        setAlertMessage(res.msg, "danger")
-        return 1
-    }
+  let res = await editor.calculate();
+
+  if (res.res === "ok") {
+    setAlertMessage("Critical path calculated", "info");
+    return 0;
+  } else {
+    setAlertMessage(res.msg, "danger");
+    return 1;
+  }
 }
 
 function clearModel(e) {
-    editor.clear();
+  editor.clear();
 }
 
 function exportModel(e) {
-    const serialized = Model.serialize(editor.model);
-    console.log(serialized);
+  const serialized = Model.serialize(editor.model);
+  console.log(serialized);
 }
 
 function importModel(e) {
-    console.log("TO DO"); 
+  console.log("TO DO");
 }
 
 function saveModel(e) {
-    const serialized = Model.serialize(editor.model);
-    localStorage.setItem("model", serialized);
+  const serialized = Model.serialize(editor.model);
+  localStorage.setItem("model", serialized);
 }
 
 function loadModel(e) {
-    editor.clear(false);
-    const serialized = localStorage.getItem("model");
-    editor.load(serialized);
-    calculate();
+  editor.clear(false);
+  const serialized = localStorage.getItem("model");
+  editor.load(serialized);
+  calculate();
 }
+
+window.onresize = function () {
+  fitStageIntoParentContainer();
+};
